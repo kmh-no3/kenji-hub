f943c65b1bd368bfdfc4c9992a18522b5f0c9338
kmh-no3 <lmbd.cs2nc.manga@gmail.com>
2025-07-31 16:23:40 +0900

    next.jsを利用する為に設定ファイルを更新

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export', // 静的HTMLを出力する
+  basePath: isProd ? '/kenji-hub' : '',
+  assetPrefix: isProd ? '/kenji-hub/' : '',
+};
 
 export default nextConfig;
